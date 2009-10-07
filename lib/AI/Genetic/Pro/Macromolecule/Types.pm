884fbfa0a0e1006d6eadac74b6fb221d0000a6c2
brunov <vecchi.b@gmail.com>
2009-10-07 14:42:43 -0300

    docs for ::Types

--- lib/AI/Genetic/Pro/Macromolecule/Types.pm
+++ lib/AI/Genetic/Pro/Macromolecule/Types.pm
@@ -1,12 +1,24 @@
 package AI::Genetic::Pro::Macromolecule::Types;
+
+# ABSTRACT: Specific types for AI::Genetic::Pro::Macromolecule
+
 use Moose;
 use MooseX::Types::Moose qw(Str);
 use MooseX::Types -declare => [qw(Probability AIGeneticPro)];
-use AI::Genetic::Pro;
 use namespace::autoclean;
 
 class_type AIGeneticPro, { class => 'AI::Genetic::Pro' };
 
 subtype Probability, as Str, where { $_ < 1 and $_ > 0 };
 
-1;
+
+__PACKAGE__->meta->make_immutable;
+
+__END__
+
+=head1 DESCRIPTION
+
+This module defines specific types and type coercions to be used by
+AI::Genetic::Pro::Macromolecule.
+
+=cut
